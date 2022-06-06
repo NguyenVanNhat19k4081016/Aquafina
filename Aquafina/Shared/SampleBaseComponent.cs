3026eb1bc9dbefbfa392582556403f0077027881
vannsanng <vansang1526@gmail.com>
2022-06-06 20:46:35 +0700

    Add project files.

---
+++ Aquafina/Shared/SampleBaseComponent.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+namespace Aquafina.Shared
+{
+    public class SampleBaseComponent : ComponentBase
+    {
+        [Inject]
+        protected SampleService SampleService { get; set; }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            base.OnAfterRender(firstRender);
+            //SampleService.Spinner?.Hide();
+            //SampleService.Spinner?.ShowModalSpinner(false);
+        }
+    }
+}
---
+++ Aquafina/Shared/SampleService.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+namespace Aquafina.Shared
+{
+    public class SampleService
+    {
+        public string WebAssetsPath { get; set; }
+        public bool IsDevice { get; set; }
+    }
+}
